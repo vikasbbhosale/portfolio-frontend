392b03d47cb4371cd51d7fd1602b2b5ca5c3f5dd
vikasbbhosasle <bhosalevikas2006@gmail.com>
2025-12-18 00:49:56 +0530

    first

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -67,7 +67,7 @@ function Contact() {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://portfolio-backend-ebon-kappa.vercel.app', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
